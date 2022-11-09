2155fae67733d0bd4a1b31e845856a04505e23f4
Thiago Kalid <thiagokkalid@gmail.com>
2022-11-09 14:22:58 -0300

    Adjusting artificial case to work with new classes

--- test/test_artifical_trajectoryestimation.py
+++ test/test_artifical_trajectoryestimation.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 from visual_encoder.trajectory_estimators import *
 from visual_encoder.svd_decomposition import *
 from PIL import Image
-
+from visual_encoder.tajectory_params import TrajectoryParams
+from visual_encoder.displacement_params import DisplacementParams
 
 def moving_average(x, w):
     return np.convolve(x, np.ones(w), 'valid') / w
@@ -22,92 +23,15 @@ img_list, xshifts, yshifts, coord = generate_artifical_shifts(base_img, x0=200, 
 print("Gerado os pontos artificiais.")
 
 # Cálculo da trajetória:
-computed_coords_pc = compute_total_trajectory(img_list, x0=200, y0=200, method='pc')
-computed_coords_svd = compute_total_trajectory(img_list, x0=200, y0=200, method='svd', window_type='Blackman-Harris')
+svd_param = DisplacementParams(method="svd", spatial_window='Blackman-Harris', frequency_window="Stone_et_al_2001")
+svd_traj = TrajectoryParams(svd_param, x0=200, y0=200)
+svd_traj.compute_total_trajectory(img_list)
+pc_param = DisplacementParams(method="pc", spatial_window=None)
+pc_traj = TrajectoryParams(pc_param, x0=200, y0=200)
+pc_traj.compute_total_trajectory(img_list)
 
 plt.imshow(base_img, cmap='gray')
 plt.plot(coord[:, 0], coord[:, 1], '-o', color='r', label='Expected')
-plt.plot(computed_coords_svd[:, 0], computed_coords_svd[:, 1], '-o', color='b', label='SVD')
-plt.plot(computed_coords_pc[:, 0], computed_coords_pc[:, 1], '-o', color='g', label='PC')
+plt.plot(svd_traj.get_coords()[:, 0], svd_traj.get_coords()[:, 1], '-o', color='b', label='SVD')
+plt.plot(pc_traj.get_coords()[:, 0], pc_traj.get_coords()[:, 1], '-o', color='g', label='PC')
 plt.legend()
-
-# # Debugando o SVD:
-# plt.figure()
-# shot = 25
-# f = img_list[shot]
-# n_jumps = 1
-# g = img_list[shot + n_jumps]
-# ideal_deltax = 3 * n_jumps
-# ideal_deltay = 2 * n_jumps
-# plt.subplot(2, 3, 1)
-# plt.imshow(f, cmap='gray')
-# plt.subplot(2, 3, 2)
-# plt.imshow(g, cmap='gray')
-# #
-# # q, Q, N = crosspower_spectrum2(f, g, method='Stone_et_al_2001')
-# qa, s, qb = svds(Q, k=1)
-# # Ângulo de qa e qb:
-# ang_qb = np.angle(qb[0, :])
-# ang_qa = np.angle(qa[:, 0])
-#
-# diff_q = np.diff(ang_qb)
-# diff_mean = np.mean(diff_q)
-# diff_std = np.std(diff_q)
-#
-# std_factor = 3
-# # diff_q2 = moving_average(diff_q, 15)
-# idx = [(np.mean(diff_q) - np.std(diff_q) * std_factor) <= d <= (np.mean(diff_q) + np.std(diff_q) * std_factor) for d
-#        in diff_q]  # ìndices para serem mantidos
-#
-# ###
-# freq_y = np.fft.fftfreq(f.shape[0])
-# freq_x = np.fft.fftfreq(f.shape[1])
-# filt_freq_y = freq_y[int(f.shape[0] // 2 - N): int(f.shape[0] // 2 + N)]
-# filt_freq_x = freq_x[int(f.shape[1] // 2 - N): int(f.shape[1] // 2 + N):]
-# filt_freq_x2 = filt_freq_x[:-1][idx]
-# filt_freq_y2 = filt_freq_y[:-1][idx]
-#
-# filt_diff = diff_q[idx]
-# # filt_ang_qb = ang_qb[:-1][idx]
-#
-# plt.subplot(2, 3, 1)
-# plt.stem(ang_qb)
-# plt.subplot(2, 3, 2)
-# plt.stem(ang_qa)
-#
-#
-# plt.title("$q_b$")
-# plt.subplot(2, 3, 4)
-# plt.stem(diff_q)
-# plt.title("Diferença de $q_b$")
-#
-# plt.subplot(2, 3, 5)
-# plt.stem(filt_diff)
-# plt.title(f"Diferença da Filtragem de $q_b$ para pontos até cte$\cdot\sigma$ onde cte={std_factor}")
-#
-# ############
-# delta = 2 * np.pi / freq_x.size
-# tanx = -np.mean(filt_diff) / delta
-# deltax = tanx
-#
-# plt.figure()
-# plt.subplot(2, 3, 1)
-# plt.imshow(f, cmap='gray')
-# plt.subplot(2, 3, 2)
-# plt.imshow(g, cmap='gray')
-# plt.subplot(2, 3, 3)
-# plt.stem(ang_qa)
-# plt.title("$ângulo de q_a$")
-# plt.subplot(2, 3, 6)
-# plt.stem(ang_qb)
-# plt.title("$ângulo de q_b$")
-#
-# plt.subplot(2, 3, 4)
-# plt.imshow(np.angle(Q), cmap='gray')
-# plt.title("Matriz Q")
-#
-# plt.subplot(2, 3, 5)
-# plt.imshow(np.angle(qa @ np.diag(s) @ qb), cmap='gray')
-# plt.title("Aproximação SVD de Q")
-#
-# plt.suptitle(f"Ruído Gaussiano Artificial de {gauss_noise} dB")
