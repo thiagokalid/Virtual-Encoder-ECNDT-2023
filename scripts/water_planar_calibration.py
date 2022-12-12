f0fdfca8ea9f10ddf34c889cd8bf7f92622c9ec1
Thiago Kalid <thiagokkalid@gmail.com>
2022-12-12 15:52:32 -0300

    Scripts related to planar contact and immersion calibration

---
+++ scripts/water_planar_calibration.py
@@ -0,0 +1,21 @@
+from visual_encoder.phase_correlation import *
+from visual_encoder.tajectory_params import TrajectoryParams
+from visual_encoder.displacement_params import DisplacementParams
+
+medium = "water"
+filename = [f"{medium}_planar_{name}_side" for name in ['shortest', 'longest']]
+data_root = "../data/calibration/"
+
+print("Starting the SVD based estimation method...")
+
+measured_longest_dist_x = np.mean([359, 358, 357])  # distance in millimeters
+measured_shortest_dist_y = np.mean([200.1, 200, 200.9])  # distance in millimeters
+svd_param = DisplacementParams(method="svd", spatial_window='Blackman-Harris', frequency_window="Stone_et_al_2001")
+svd_traj = TrajectoryParams(svd_param)
+y_res, x_res = svd_traj.calibrate(data_root,
+                                  filename_list=filename,
+                                  measured_coords=[measured_shortest_dist_y, measured_longest_dist_x],
+                                  n_images=[1481, 1994]
+                                  )
+
+# Result: x_res, y_res = (0.020522031707206376, 0.025899312847316048)
