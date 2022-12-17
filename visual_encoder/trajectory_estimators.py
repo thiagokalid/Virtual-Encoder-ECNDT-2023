2a7d5a23779745a400e0d16df8b2d2991ffa97c1
Thiago Kalid <thiagokkalid@gmail.com>
2022-12-17 11:49:46 -0300

    Beginning of integration between quaternion data and shift estimator

--- visual_encoder/trajectory_estimators.py
+++ visual_encoder/trajectory_estimators.py
@@ -3,6 +3,7 @@ import os
 from visual_encoder.dip_utils import gaussian_noise, salt_and_pepper, apply_window
 from visual_encoder.phase_correlation import pc_method
 from visual_encoder.svd_decomposition import svd_method
+from scipy.spatial.transform import Rotation as R
 
 def compute_new_position(deltax, deltay, x0, y0):
     xf = x0 + deltax
@@ -35,11 +36,12 @@ def compute_total_trajectory(img_list, traj_params):
         x0, y0 = positions[i, :]
     return positions
 
+
 def compute_total_trajectory_path(data_root, n_images, traj_params, n_beg=1):
     positions = np.zeros((n_images, 2))
     x0, y0 = positions[0, :] = traj_params.get_init_coord()
     for i in range(n_beg + 1, n_images + n_beg):
-        img_0 = get_img(i-1, data_root)
+        img_0 = get_img(i - 1, data_root)
         img_f = get_img(i, data_root)
         j = i - n_beg
         positions[j, :] = compute_trajectory(img_0, img_f, x0, y0, traj_params)
@@ -57,6 +59,7 @@ def get_img(i, data_root):
     img_rgb = np.array(myImage)
     img_gray = rgb2gray(img_rgb)
     return img_gray
+
 
 def generate_artifical_shifts(base_image, width=None, height=None, x0=0, y0=0, xshifts=None, yshifts=None, steps=100,
                               gaussian_noise_db=None, salt_pepper_noise_prob=None):
@@ -96,3 +99,26 @@ def generate_artifical_shifts(base_image, width=None, height=None, x0=0, y0=0, x
         img_list.append(shifted_img)
     salt_and_pepper(shifted_img)
     return img_list, xshifts, yshifts, coordinates
+
+
+def convert_to_3d(coords_2d, quaternion_vector):
+    coords_3d = np.zeros(shape=(coords_2d.shape[0], 3))
+    coords_3d[0, :3] = coords_2d[0, :]
+    for i in range(1, coords_2d.shape[0]):
+        delta_2d = coords_2d[i] - coords_2d[i-1]
+        quat = quaternion_vector[i]
+        r = R.from_quat(quat)
+        delta_3d = r.apply(delta_2d)
+        coords_3d[i, :] = coords_3d[i-1, :] + delta_3d
+    return coords_3d
+
+def get_quat_data(data_root, filename="quat_data", n=995):
+    quat_data = np.zeros(shape=(n, 4))
+    with open(data_root + "/" + filename + '.txt', 'r') as f:
+        for i, line in enumerate(f):
+            corrected_line = line.replace("(", "").replace(")", "").replace(" ", "").replace("\n", "").split(',')
+            if "None" in corrected_line:
+                corrected_line = previous_line
+            quat_data[i, :] = np.array([float(x) for x in corrected_line])
+            previous_line = corrected_line
+    return quat_data
