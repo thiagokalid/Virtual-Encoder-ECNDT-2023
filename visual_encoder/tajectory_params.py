d7ed956bebd5ad7986578d0860fc3c5c84e785ed
Thiago Kalid <thiagokkalid@gmail.com>
2022-12-17 11:53:12 -0300

    3D trajectory implementation

--- visual_encoder/tajectory_params.py
+++ visual_encoder/tajectory_params.py
@@ -1,7 +1,7 @@
 from visual_encoder.displacement_params import DisplacementParams
-from visual_encoder.trajectory_estimators import compute_total_trajectory, compute_total_trajectory_path
+from visual_encoder.trajectory_estimators import compute_total_trajectory, compute_total_trajectory_path, convert_to_3d
 from visual_encoder.phase_correlation import crosspower_spectrum
-from visual_encoder.trajectory_estimators import get_img
+from visual_encoder.trajectory_estimators import get_img, get_quat_data
 from visual_encoder.svd_decomposition import phase_unwrapping, linear_regression
 from scipy.sparse.linalg import svds
 import numpy as np
@@ -10,11 +10,12 @@ import numpy as np
 # This class contains all parameters required in the global displacement estimation algorithm
 
 class TrajectoryParams(DisplacementParams):
-    def __init__(self, disp_params: DisplacementParams, x0=0, y0=0):
+    def __init__(self, disp_params: DisplacementParams, x0=0, y0=0, z0=0):
         super().__init__(method=disp_params.method, spatial_window=disp_params.spatial_window,
                          frequency_window=disp_params.frequency_window)
         self.x0 = x0
         self.y0 = y0
+        self.z0 = z0
         self.coords = None
         self.n_shots = None
 
@@ -56,8 +57,16 @@ class TrajectoryParams(DisplacementParams):
     def compute_total_trajectory(self, img_list):
         self.coords = compute_total_trajectory(img_list, self)
 
-    def compute_total_trajectory_path(self, data_root, n_images, n_beg=1):
-        self.coords = compute_total_trajectory_path(data_root, n_images, self, n_beg=n_beg)
+    def compute_total_trajectory_path(self, data_root, n_images, n_beg=1, traj_type="2d"):
+        coords_2d = compute_total_trajectory_path(data_root, n_images, self, n_beg=n_beg)
+        if traj_type == "2d":
+            self.coords = coords_2d
+        elif traj_type =="3d":
+            quaternion_data = get_quat_data(data_root)
+            coords_3d = convert_to_3d(coords_2d, quaternion_data)
+        else:
+            pass
+        return coords_2d
 
     def calibrate(self, data_root, filename_list, measured_coords, n_images):
         # First element in filename_list is related to X axis calibration, then Y shift calibration.
