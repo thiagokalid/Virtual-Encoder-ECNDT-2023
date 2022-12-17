d0b98a1340af708e4b32c442ba5be68c6669a400
Thiago Kalid <thiagokkalid@gmail.com>
2022-12-17 11:50:15 -0300

    Little refactor in ideal_lowpass

--- visual_encoder/dsp_utils.py
+++ visual_encoder/dsp_utils.py
@@ -4,11 +4,11 @@ from numpy.fft import fft2, fftshift
 # Utilities associated with digital signal processing (DSP).
 
 
-def ideal_lowpass(I, method='Stone_et_al_2001'):
+def ideal_lowpass(I, factor=0.6, method='Stone_et_al_2001'):
     if method == 'Stone_et_al_2001':
-        m = 0.6 * I.shape[0]/2
-        n = 0.6 * I.shape[1]/2
-        N = np.min([m,n])
+        m = factor * I.shape[0]/2
+        n = factor * I.shape[1]/2
+        N = np.min([m, n])
         I = I[int(I.shape[0] // 2 - N): int(I.shape[0] // 2 + N),
             int(I.shape[1] // 2 - N): int(I.shape[1] // 2 + N)]
         return I
