feb0cba27fe3f0483ae1d838750f9efc4838ae84
Thiago Kalid <thiagokkalid@gmail.com>
2022-12-19 20:41:48 -0300

    Script to convert mp4 video to image files

---
+++ scripts/video_generation/convert_to_image.py
@@ -0,0 +1,14 @@
+import cv2
+video_type = ["calibration_x", "calibration_y", "single_x", "single_y", "closed_loop"]
+for i in range(len(video_type)):
+  data_root = f"../../media/planar/water/{video_type[i]}"
+  vidcap = cv2.VideoCapture(data_root + ".mp4")
+  success, image = vidcap.read()
+  count = 1
+
+
+  while success:
+    cv2.imwrite(data_root + "_video/image_%d.jpg" % count, image)
+    success, image = vidcap.read()
+    print('Saved image ', count)
+    count += 1
