71c48c855d24e8f9fc26d8c889a4ffb1e86e41a6
Thiago Kalid <thiagokkalid@gmail.com>
2022-12-19 21:26:08 -0300

    Update generate_table_1 to new resolution value

--- scripts/generate_table_1.py
+++ scripts/generate_table_1.py
@@ -2,12 +2,11 @@ from visual_encoder.phase_correlation import *
 from visual_encoder.tajectory_params import TrajectoryParams
 from visual_encoder.displacement_params import DisplacementParams
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Geometry : Plane
 # Test type: Contact (air)
 
-# Calibration output:
-x_res_air, y_res_air = (0.02151467169232321, 0.027715058926976663)  # unit: mm / pixels
 # Measured planar specimen dimensions:
 measured_longest_dist_x = np.mean([359, 358, 357])  # distance in millimeters
 measured_shortest_dist_y = np.mean([200.1, 200, 200.9])  # distance in millimeters
@@ -16,7 +15,9 @@ filename = [f"air_{name}" + "/" for name in ['closed_loop', 'single_x', 'single_
 data_root = "../data/planar/"
 print("Starting the SVD based estimation method...")
 # General SVD related parameters:
-svd_param = DisplacementParams(method="svd", spatial_window='Blackman-Harris', frequency_window="Stone_et_al_2001")
+svd_param = DisplacementParams(method="svd", spatial_window='Blackman-Harris', frequency_window="Stone_et_al_2001",
+                               xy_resolution=(0.02151467169232321, 0.027715058926976663), resolution_unit="mm/pixels",
+                               rotation_correction=-0.034107828176162376)
 
 # Medium: Air (Contact)
 # Geometry: Planar
@@ -48,7 +49,7 @@ yrange = np.arange(0, measured_shortest_dist_y)
 plt.suptitle("Ensaios fora d'gua")
 factor = .3
 plt.subplot(1, 3, 1)
-plt.plot(single_x_air.coords[:, 0] * x_res_air, single_x_air.coords[:, 1] * y_res_air * (-1), 'o', color="#FF1F5B")
+plt.plot(single_x_air.coords[:, 0], single_x_air.coords[:, 1] * (-1), 'o', color="#FF1F5B")
 plt.title("Single-X")
 plt.plot(xrange, xrange * 0, linewidth=2, color="#009ADE", label="True")
 plt.xlim([measured_longest_dist_x * (-factor), measured_longest_dist_x * (1+factor)])
@@ -56,14 +57,14 @@ plt.axis('equal')
 plt.grid()
 plt.legend()
 plt.subplot(1, 3, 2)
-plt.plot(single_y_air.coords[:, 0] * x_res_air, single_y_air.coords[:, 1] * y_res_air * (-1), 'o', color="#FF1F5B")
+plt.plot(single_y_air.coords[:, 0], single_y_air.coords[:, 1] * (-1), 'o', color="#FF1F5B")
 plt.title("Single-Y")
 plt.plot(yrange * 0, yrange, linewidth=2, color="#009ADE", label="True")
 plt.axis('equal')
 plt.grid()
 plt.legend()
 plt.subplot(1, 3, 3)
-plt.plot(closed_loop_air.coords[:, 0] * x_res_air, closed_loop_air.coords[:, 1] * y_res_air * (-1), 'o', color="#FF1F5B")
+plt.plot(closed_loop_air.coords[:, 0], closed_loop_air.coords[:, 1] * (-1), 'o', color="#FF1F5B")
 plt.title("Closed-loop")
 plt.ylim([measured_shortest_dist_y * (-factor), measured_shortest_dist_y * (1+factor)])
 factor = .15
@@ -78,10 +79,9 @@ plt.grid()
 
 # Geometry : Plane
 # Test type: Immersion (water)
-
-# Calibration output:
-x_res_water, y_res_water = (0.020354609115638543, 0.025723094098896563)
-
+svd_param = DisplacementParams(method="svd", spatial_window='Blackman-Harris', frequency_window="Stone_et_al_2001",
+                               xy_resolution=(0.02035460911563854, 0.025723094098896573), resolution_unit="mm/pixels",
+                               rotation_correction=-0.02428690717359043)
 filename = [f"water_{name}" + "/" for name in ['closed_loop', 'single_x', 'single_y']]
 data_root = "../data/planar/"
 print("Starting the SVD based estimation method...")
@@ -117,7 +117,7 @@ yrange = np.arange(0, measured_shortest_dist_y)
 plt.suptitle("Ensaios dentro d'gua")
 factor = .3
 plt.subplot(1, 3, 1)
-plt.plot(single_x_water.coords[:, 0] * x_res_water, single_x_water.coords[:, 1] * y_res_water * (-1), 'o', color="#FF1F5B")
+plt.plot(single_x_water.coords[:, 0], single_x_water.coords[:, 1] * (-1), 'o', color="#FF1F5B")
 plt.plot(xrange, xrange * 0, linewidth=2, color="#009ADE", label="True")
 plt.title("Single-X")
 plt.xlim([measured_longest_dist_x * (-factor), measured_longest_dist_x * (1+factor)])
@@ -126,7 +126,7 @@ plt.grid()
 plt.legend()
 
 plt.subplot(1, 3, 2)
-plt.plot(single_y_water.coords[:, 0] * x_res_water, single_y_water.coords[:, 1] * y_res_water * (-1), 'o', color="#FF1F5B")
+plt.plot(single_y_water.coords[:, 0], single_y_water.coords[:, 1] * (-1), 'o', color="#FF1F5B")
 plt.plot(yrange * 0, yrange, linewidth=2, color="#009ADE", label="True")
 plt.title("Single-Y")
 plt.axis('equal')
@@ -134,7 +134,7 @@ plt.grid()
 plt.legend()
 
 plt.subplot(1, 3, 3)
-plt.plot(closed_loop_water.coords[:, 0] * x_res_water, closed_loop_water.coords[:, 1] * y_res_water * (-1), 'o', color="#FF1F5B")
+plt.plot(closed_loop_water.coords[:, 0], closed_loop_water.coords[:, 1] * (-1), 'o', color="#FF1F5B")
 plt.title("Closed-loop")
 plt.ylim([measured_shortest_dist_y * (-factor), measured_shortest_dist_y * (1+factor)])
 factor = .15
@@ -147,3 +147,9 @@ plt.axis('equal')
 plt.grid()
 plt.legend()
 
+df_plane_single_x = pd.DataFrame({'x': [single_x_air.coords[-1, 0], single_x_water.coords[-1, 0], measured_longest_dist_x],
+                                  'y': [-1 * single_x_air.coords[-1, 1], -1 * single_x_water.coords[-1, 1], 0]})
+df_plane_single_y = pd.DataFrame({'x': [single_y_air.coords[-1, 0], single_y_water.coords[-1, 0], 0],
+                                  'y': [-1 * single_y_air.coords[-1, 1], -1 * single_y_water.coords[-1, 1], measured_shortest_dist_y]})
+df_plane_closed_loop = pd.DataFrame({'x': [closed_loop_air.coords[-1, 0], closed_loop_water.coords[-1, 0], 0],
+                                  'y': [-1 * closed_loop_air.coords[-1, 1], -1 * closed_loop_water.coords[-1, 1], 0]})
