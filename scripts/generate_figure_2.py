4b1a0096443f0dba08e81e1f24765e4210661a8e
Thiago Kalid <thiagokkalid@gmail.com>
2023-02-09 10:55:51 -0300

    Corrections on Figure 2 generation.

--- scripts/generate_figure_2.py
+++ scripts/generate_figure_2.py
@@ -60,63 +60,95 @@ x2 = r
 y = pv_unwrapped
 mu2, c2 = linear_regression(x2, y)
 
+# Values for custom xlim and ylim:
+amplitude = np.abs(np.max(pv_unwrapped)) + np.abs(np.min(pv_unwrapped))
+middle_pu = np.mean(pu_unwrapped)
+middle_pv = np.mean(pv_unwrapped)
+
 fig, axis = plt.subplots(nrows=2, ncols=7)
 
 axis[0, 0].imshow(f, cmap="gray")
 axis[0, 0].set_title("$f_n(x,y)$")
+axis[0, 0].tick_params(bottom=False, left=False)
+axis[0, 0].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 axis[1, 0].imshow(g, cmap="gray")
 axis[1, 0].set_title("$f_{n-1}(x,y)$")
+axis[1, 0].tick_params(bottom=False, left=False)
+axis[1, 0].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 axis[0, 1].imshow(np.log10(np.abs(F) + 1), cmap="gray")
 axis[0, 1].set_title("$log_{10}(|FFT(f_{n}(x,y)| + 1)$")
+axis[0, 1].tick_params(bottom=False, left=False)
+axis[0, 1].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 axis[1, 1].imshow(np.log10(np.abs(G) + 1), cmap="gray")
 axis[1, 1].set_title("$log_{10}(|FFT(f_{n-1}(x,y)| + 1)$")
+axis[1, 1].tick_params(bottom=False, left=False)
+axis[1, 1].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 axis[0, 2].imshow(np.log10(np.abs(Q)), cmap='gray')
 axis[0, 2].set_title("$Q_n$")
+axis[0, 2].tick_params(bottom=False, left=False)
+axis[0, 2].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 axis[1, 2].imshow(np.log10(np.abs(Q_wind)), cmap='gray')
 axis[1, 2].set_title("$Q_n \cdot w(x,y)$")
+axis[1, 2].tick_params(bottom=False, left=False)
+axis[1, 2].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 axis[0, 3].plot(np.real(qu), '-o', markersize=3, label="$Re(q_u)$", color="#FF1F5B")
 axis[0, 3].plot(np.imag(qu), '-o', markersize=3, label="$Im(q_u)$", color="#FFC61E")
 axis[0, 3].set_title("$q_u(u,v)$")
 axis[0, 3].legend()
-
-
+axis[0, 3].tick_params(bottom=False, left=False)
+axis[0, 3].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 
 axis[1, 3].plot(np.real(qv), '-o', markersize=3, label="$Re(q_v)$", color="#FF1F5B")
 axis[1, 3].plot(np.imag(qv), '-o', markersize=3, label="$Im(q_v)$", color="#FFC61E")
 axis[1, 3].set_title("$q_v(u,v)$")
 axis[1, 3].legend()
-
+axis[1, 3].tick_params(bottom=False, left=False)
+axis[1, 3].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 axis[0, 4].plot(pu, '-o', markersize=4, color="#FF1F5B")
 axis[0, 4].set_title("$p_u(u,v)$")
-
-
-
-
-
+axis[0, 4].tick_params(bottom=False, left=False)
+axis[0, 4].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 axis[1, 4].plot(pv, '-o', markersize=4, color="#FF1F5B")
 axis[1, 4].set_title("$p_v(u,v)$")
+axis[1, 4].tick_params(bottom=False, left=False)
+axis[1, 4].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 axis[0, 5].plot(pu_unwrapped, 'o', markersize=4, color="#FF1F5B")
 axis[0, 5].set_title("unwrap($p_u(u,v)$)")
-
-
+axis[0, 5].set_ylim([middle_pu - amplitude/1.8, middle_pu + amplitude/1.8])
+axis[0, 5].tick_params(bottom=False, left=False)
+axis[0, 5].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 axis[1, 5].plot(pv_unwrapped, 'o', markersize=4, color="#FF1F5B")
 axis[1, 5].set_title("unwrap($p_v(u,v)$)")
+axis[1, 5].set_ylim([middle_pv - amplitude/1.8, middle_pv + amplitude/1.8])
+axis[1, 5].tick_params(bottom=False, left=False)
+axis[1, 5].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 x_span = np.linspace(0, pu_unwrapped.size, pu_unwrapped.size)
 axis[0, 6].plot(x1, pu_unwrapped, 'o', markersize=4, color="#FF1F5B")
 axis[0, 6].plot(x1, x1 * mu1 + c1, color='#009ADE', linewidth=2)
 axis[0, 6].set_title("Linear fit of $p_u$")
+axis[0, 6].set_title("unwrap($p_u(u,v)$)")
+axis[0, 6].set_ylim([middle_pu - amplitude/1.8, middle_pu + amplitude/1.8])
+axis[0, 6].tick_params(bottom=False, left=False)
+axis[0, 6].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
+
+axis[1, 6].plot(x1, pv_unwrapped, 'o', markersize=4, color="#FF1F5B")
+axis[1, 6].plot(x2, x2 * mu2 + c2, color='#009ADE', linewidth=2)
+axis[1, 6].set_title("Linear fit of $p_v$")
+axis[1, 6].set_ylim([middle_pv - amplitude/1.8, middle_pv + amplitude/1.8])
+axis[1, 6].tick_params(bottom=False, left=False)
+axis[1, 6].set(xlabel=None, xticklabels=[], ylabel=None, yticklabels=[])  # remove the axis label
 
 # fig = plt.figure(figsize=(3, 3))
 # ax = plt.gca()
@@ -133,11 +165,6 @@ axis[0, 6].set_title("Linear fit of $p_u$")
 # plt.savefig("../figs/Fig2.png", dpi=300, format=None, metadata=None,
 #         bbox_inches="tight"
 #        )
-
-axis[1, 6].plot(x1, pv_unwrapped, 'o', markersize=4, color="#FF1F5B")
-axis[1, 6].plot(x2, x2 * mu2 + c2, color='#009ADE', linewidth=2)
-axis[1, 6].set_title("Linear fit of $p_v$")
-
 
 # fig = plt.figure(figsize=(3, 3))
 # ax = plt.gca()
