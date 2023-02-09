c9087342d1f6ae43d756f30e84b7492fd3590f24
Thiago Kalid <thiagokkalid@gmail.com>
2023-02-09 20:27:36 -0300

    Latest video generation script

--- scripts/video_generation/generate_video.py
+++ scripts/video_generation/generate_video.py
@@ -3,6 +3,10 @@ from matplotlib.animation import FFMpegWriter
 from visual_encoder.trajectory_estimators import get_img
 from visual_encoder.tajectory_params import TrajectoryParams
 from visual_encoder.displacement_params import DisplacementParams
+import matplotlib
+gui_env = ['GTK3Agg', 'GTK3Cairo', 'GTK4Agg', 'GTK4Cairo', 'MacOSX', 'nbAgg', 'QtAgg', 'QtCairo', 'Qt5Agg', 'Qt5Cairo', 'TkAgg', 'TkCairo', 'WebAgg', 'WX', 'WXAgg', 'WXCairo', 'agg', 'cairo', 'pdf', 'pgf', 'ps', 'svg', 'template']
+
+matplotlib.use(gui_env[8], force=True)
 
 # Where to get the data:
 geometries = ["planar", "cylindrical"]
@@ -13,9 +17,7 @@ data_root = f"../../data/{geometries[0]}/{media[0]}_{excurtion_types[chosen_type
 media_root = f"../../media/{geometries[0]}/{media[0]}/{excurtion_types[chosen_type]}_video/"
 
 # General SVD related parameters:
-svd_param = DisplacementParams(method="svd", spatial_window='Blackman-Harris', frequency_window="Stone_et_al_2001",
-                               xy_resolution=(0.02151467169232321, 0.027715058926976663), resolution_unit="mm/pixels",
-                               rotation_correction=-0.034107828176162376)
+svd_param = DisplacementParams(method="svd", spatial_window='Blackman-Harris', frequency_window="Stone_et_al_2001")
 
 # Medium: Air (Contact)
 # Geometry: Planar
@@ -26,7 +28,7 @@ svd_traj.compute_total_trajectory_path(data_root, n_images=1496)
 print("End of closed-loop...")
 
 # Parâmetros de geração de vídeo:
-results_path = "../../Virtual-Encoder-ECNDT-2023/results/video/"
+results_path = "../../results/video/"
 videotitle = "Video_v01"
 metadata = dict(title=results_path + videotitle, artist='Matplotlib',
                 comment='Movie support!')
@@ -45,7 +47,7 @@ if generate_video == True:
             plt.suptitle(f"Shot = {shot}")
             plt.subplot(2, 2, 1)
             curr_img = get_img(shot, data_root)
-            plt.imshow(curr_img[::-1, :], cmap='gray',
+            plt.imshow(curr_img, cmap='gray',
                        extent=[svd_traj.get_coords(shot)[0],  # Esquerda X
                                svd_traj.get_coords(shot)[0] + curr_img.shape[1],  # Direita X
                                svd_traj.get_coords(shot)[1],  # Topo Y
@@ -79,3 +81,37 @@ if generate_video == True:
             writer.grab_frame()
             plt.clf()
     print("Fim do loop de geração de vídeo.")
+else:
+    plt.suptitle(f"Shot = {shot}")
+    plt.subplot(2, 2, 1)
+    curr_img = get_img(shot, data_root)
+    plt.imshow(curr_img, cmap='gray',
+               extent=[svd_traj.get_coords(shot)[0],  # Esquerda X
+                       svd_traj.get_coords(shot)[0] + curr_img.shape[1],  # Direita X
+                       svd_traj.get_coords(shot)[1],  # Topo Y
+                       svd_traj.get_coords(shot)[1] + curr_img.shape[0]]  # Base Y
+               )
+    plt.xlim([svd_traj.get_coords(shot)[0], svd_traj.get_coords(shot)[0] + curr_img.shape[1]])
+    plt.ylim([svd_traj.get_coords(shot)[1], svd_traj.get_coords(shot)[1] + curr_img.shape[0]])
+    offset_y = curr_img.shape[1] / 2
+    offset_x = curr_img.shape[1] / 2
+
+    plt.plot(svd_traj.get_coords()[:shot + 1][:, 0] + offset_x,
+             svd_traj.get_coords()[:shot + 1][:, 1] + offset_y,
+             ':o',
+             color='r', label='SVD')
+    plt.legend()
+
+    print(f"frame = {shot + 1}")
+
+    plt.subplot(2, 2, 2)
+    plt.plot(svd_traj.get_coords()[:shot + 1][:, 0] + offset_x,
+             svd_traj.get_coords()[:shot + 1][:, 1] + offset_y,
+             ':o',
+             color='r', label='SVD')
+
+    plt.subplot(2, 2, 4)
+    plt.title("Recording")
+    photo = get_img(shot, media_root, rgb=True)
+    plt.imshow(photo)
+    plt.axis(False)
